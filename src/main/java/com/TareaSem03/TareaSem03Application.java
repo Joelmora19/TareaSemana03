f3551735a3f21a11aae8c0f1824b78ea2fc0e976
Joelmora19 <108998021+Joelmora19@users.noreply.github.com>
2023-02-08 16:42:17 -0600

    Tarea Semana03

---
+++ src/main/java/com/TareaSem03/TareaSem03Application.java
@@ -0,0 +1,13 @@
+package com.TareaSem03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaSem03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaSem03Application.class, args);
+	}
+
+}
